1b0c023d3e05fc6203e071b47a40c5588bb90c8c
EternalTimes <Tirrammissu@outlook.com>
2026-04-29 09:47:08 +0800

    Initial Commit

---
+++ Project CageTests/Project_CageTests.swift
@@ -0,0 +1,18 @@
+//
+//  Project_CageTests.swift
+//  Project CageTests
+//
+//  Created by Predix on 4/29/26.
+//
+
+import Testing
+
+struct Project_CageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
